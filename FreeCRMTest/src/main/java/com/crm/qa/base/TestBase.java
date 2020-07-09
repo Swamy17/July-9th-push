8fc76649c4f75c54602f6d3d6352c969dc5540f4
Swamy17 <53607513+Swamy17@users.noreply.github.com>
2020-07-09 18:43:18 +0530

    Update TestBase.java
    comments from github

--- FreeCRMTest/src/main/java/com/crm/qa/base/TestBase.java
+++ FreeCRMTest/src/main/java/com/crm/qa/base/TestBase.java
@@ -41,7 +41,7 @@ public class TestBase {
 	
 	public static void initialization(){
 		String browserName = prop.getProperty("browser");
-		
+		System.out.println("Adding the comments from GITHUB");
 		if(browserName.equals("chrome")){
 			System.setProperty("webdriver.chrome.driver", "D:\\Chrome Driver latest\\chromedriver_win32\\chromedriver.exe");	
 			driver = new ChromeDriver(); 
